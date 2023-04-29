fd77029dc4ecc9239ac4daed6471964f79135161
Bob Oyier <boboyier92@gmail.com>
2023-04-29 22:19:59 +0300

    OOP in C#. My MySuperBank mini-project.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using MySuperBank;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var account = new BankAccount("Bob", 10000);
+        Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+        account.MakeWithdrawal(120, DateTime.Now, "Rent Money.");
+        Console.WriteLine($"Your account balance is now {account.Balance}");
+    }
+}
